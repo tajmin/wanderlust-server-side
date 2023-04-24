ca6e4133a556c7e33ce7448b3831683f45a7f52b
Md. Tajminur Rahman <tajminur.aiub@gmail.com>
2023-04-24 10:40:48 +0600

    update - mongodb uri, readme and package-lock

---
+++ README.md
@@ -0,0 +1,19 @@
+# Wanderlust - Travel Management Website
+
+## Live site [Wanderlust - hosted on firebase](https://wanderlust-c746b.web.app/).
+
+### Wanderlust in Brief
+
+Wanderlust is a mobile-friendly SPA website for companies that has anything to do with travel and vacations, including but not limited to Travel Agents, Travel eCommerce, Tour Guides, Travel Company, Travel Club, Tour Operator, Travel listing Directory, Travel Deals, and Tour Packages. Wanderlust has a list of travel plans/vacation getaways section on the home page and also features user authentication, booking steps for authenticated users, My Orders page for booking related information and lastly, Manage Orders page for managing all sorts of booking requested by registered users. Wanderslust is a MERN stack project that stores and loads data from MongoDB cloud database and uses RESTful services.
+
+### Key Points
+
+- Showcases vacation plans offered by the company in brief and dynamically with each plan having a detailed page for booking.
+- Google Sign-in method to facilitate user authentication and help book vacation plans available to only logged in users.
+- My Orders page for the list of bookings made by the user with an option to cancel each booking.
+- Bookings made by all of the users with an option to approve each booking available on the Manage Order page.
+- Add Tour Plan page for adding a new vacation plan.
+
+### Built with
+
+`Create-React-App` `React Router Dom` `React Hook Form` `Tailwind CSS` `Tailwind CSS Forms` `Font Awesome` `HeroIcon` `Headless UI` `Firebase` `Node` `Express` `MongoDB`
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const cors = require('cors');
-require('dotenv').config();
-const ObjectId = require('mongodb').ObjectId;
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,98 +10,104 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7xlcz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7xlcz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try {
-        await client.connect();
-        const database = client.db('wanderlust');
-        const tourplanCollection = database.collection('tourplans');
-        const bookingCollection = database.collection('bookings');
+  try {
+    await client.connect();
+    const database = client.db("wanderlust");
+    const tourplanCollection = database.collection("tourplans");
+    const bookingCollection = database.collection("bookings");
 
-        //GET: Get tour plans
-        app.get('/tour-plans', async (req, res) => {
-            const cursor = tourplanCollection.find({});
-            const result = await cursor.toArray();
-            res.send(result);
-        });
+    //GET: Get tour plans
+    app.get("/tour-plans", async (req, res) => {
+      const cursor = tourplanCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-        //GET: Get tour plans by Id
-        app.get('/plan-details/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) }
-            const result = await tourplanCollection.findOne(query);
-            res.json(result);
-        });
+    //GET: Get tour plans by Id
+    app.get("/plan-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await tourplanCollection.findOne(query);
+      res.json(result);
+    });
 
-        //GET: Get all bookings
-        app.get('/booking', async (req, res) => {
-            const cursor = bookingCollection.find({});
-            const result = await cursor.toArray();
-            res.send(result);
-        });
+    //GET: Get all bookings
+    app.get("/booking", async (req, res) => {
+      const cursor = bookingCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-        //POST: Post new tourplan
-        app.post('/tour-plans', async (req, res) => {
-            const newPlan = req.body;
-            console.log(newPlan)
-            const result = await tourplanCollection.insertOne(newPlan);
-            res.json(result);
-        });
+    //POST: Post new tourplan
+    app.post("/tour-plans", async (req, res) => {
+      const newPlan = req.body;
+      console.log(newPlan);
+      const result = await tourplanCollection.insertOne(newPlan);
+      res.json(result);
+    });
 
-        //POST: Book tourplan
-        app.post('/booking', async (req, res) => {
-            const bookingDetails = req.body;
-            const result = await bookingCollection.insertOne(bookingDetails);
-            res.json(result);
-        });
+    //POST: Book tourplan
+    app.post("/booking", async (req, res) => {
+      const bookingDetails = req.body;
+      const result = await bookingCollection.insertOne(bookingDetails);
+      res.json(result);
+    });
 
-        //POST: Find bookings by user email
-        app.post('/my-bookings', async (req, res) => {
-            const userEmail = req.body.email;
-            const query = { email: userEmail };
-            const products = await bookingCollection.find(query).toArray();
-            res.json(products);
-        });
+    //POST: Find bookings by user email
+    app.post("/my-bookings", async (req, res) => {
+      const userEmail = req.body.email;
+      const query = { email: userEmail };
+      const products = await bookingCollection.find(query).toArray();
+      res.json(products);
+    });
 
-        //PUT: Approve booking
-        app.put('/booking', async (req, res) => {
-            const updatedBooking = req.body;
-            console.log(updatedBooking);
-            const filter = { _id: ObjectId(updatedBooking.bookingId) };
-            const options = { upsert: true };
-            const updateDoc = {
-                $set: {
-                    status: updatedBooking.status
-                },
-            };
+    //PUT: Approve booking
+    app.put("/booking", async (req, res) => {
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const filter = { _id: ObjectId(updatedBooking.bookingId) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status,
+        },
+      };
 
-            const result = await bookingCollection.updateOne(filter, updateDoc, options);
-            res.json(result);
-        })
+      const result = await bookingCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
 
-        //DELETE: Cancel Booking
-        app.delete('/booking/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await bookingCollection.deleteOne(query);
-            res.json(result);
-        });
-
-    }
-    finally {
-        // await client.close();
-    }
+    //DELETE: Cancel Booking
+    app.delete("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Server Running')
+app.get("/", (req, res) => {
+  res.send("Server Running");
 });
 
 app.listen(port, () => {
-    console.log('Listening at:', port);
-})
+  console.log("Listening at:", port);
+});
 
 // git push heroku main
